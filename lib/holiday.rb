f41e3122f3dd611771c78a3ad4cc69a5217781c8
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-27 20:51:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -97,9 +97,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  
+   bbq_holidays = []
   holiday_hash.collect do |season_key, data|
-    bbq_holidays = []
+   
     data.collect do |holiday_key, supply_array|
       if (supply_array.include?("BBQ") && bbq_holidays.include?(holiday_key) == false) 
         bbq_holidays.push(holiday_key)
