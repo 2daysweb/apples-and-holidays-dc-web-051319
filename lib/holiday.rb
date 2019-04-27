21d0deb63238c387c8879423d23d0e97d1ea10fb
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-27 19:55:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -105,7 +105,7 @@ def all_holidays_with_bbq(holiday_hash)
         bbq_holidays.push(holiday_key)
       end
       bbq_holidays.flatten
-      
+      bbq_holidys.reject {|e| e.is_empty?}
     end
   end
 end
