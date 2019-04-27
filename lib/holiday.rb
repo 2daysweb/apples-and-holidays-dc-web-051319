37a4e7688217cab9fddea2a0cd85bb5f89d15ca2
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-27 20:21:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -108,7 +108,10 @@ def all_holidays_with_bbq(holiday_hash)
   end
   bbq_holidays.collect do |array|
        array.reject {|array| 
-       if(array.array.to_s.empty?}
+       if(array.is_a(Symbol)
+         bbq_holidays_flattened.push(array)
+       else
+         array.to_s.empty?
     end
 end
 
