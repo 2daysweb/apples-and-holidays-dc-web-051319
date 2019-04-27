1de13d4576433e07318c157bf9204ff8e794aa46
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-27 20:05:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -103,7 +103,7 @@ def all_holidays_with_bbq(holiday_hash)
     data.collect do |holiday_key, supply_array|
       if (supply_array.include?("BBQ") && bbq_holidays.include?(holiday_key) == false)
         bbq_holidays.push(holiday_key)
-        bbq_holidays.flatten.compact
+        bbq_holidays.flatten
       end
      bbq_holidays
     end
